7d0551051ffe1da49ba6adef555f7b573f3b204f
SahilPrasad <sahil.prasad@zestiot.io>
2025-09-11 00:55:07 +0530

    fixed computer loading time

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -89,7 +89,7 @@ const Computers = ({isMobile}) => {
 
     return (
         <mesh>
-            <hemisphereLight intensity={0.15} groundColor='black'/>
+      <hemisphereLight intensity={0.15} groundColor="black" />
             <spotLight
                 position={[-20, 50, 10]}
                 angle={0.12}
@@ -111,31 +111,27 @@ const Computers = ({isMobile}) => {
 
 const ComputersCanvas = () => {
     const [isMobile, setIsMobile] = useState(false);
+  const [showCanvas, setShowCanvas] = useState(false);
+
+  useEffect(() => {
+    // Set delay for canvas render
+    const timer = setTimeout(() => setShowCanvas(true), 0);
+    return () => clearTimeout(timer);
+  }, []);
 
     useEffect(() => {
-        // Add a listener for changes to the screen size
         const mediaQuery = window.matchMedia("(max-width: 500px)");
-
-        // Set the initial value of the `isMobile` state variable
         setIsMobile(mediaQuery.matches);
-
-        // Define a callback function to handle changes to the media query
-        const handleMediaQueryChange = (event) => {
-            setIsMobile(event.matches);
-        };
-
-        // Add the callback function as a listener for changes to the media query
+    const handleMediaQueryChange = (event) => setIsMobile(event.matches);
         mediaQuery.addEventListener("change", handleMediaQueryChange);
-
-        // Remove the listener when the component is unmounted
-        return () => {
-            mediaQuery.removeEventListener("change", handleMediaQueryChange);
-        };
+    return () => mediaQuery.removeEventListener("change", handleMediaQueryChange);
     }, []);
+
+  if (!showCanvas) return null;
 
     return (
         <Canvas
-            frameloop='demand'
+      frameloop="demand"
             shadows
             dpr={[1, 2]}
             camera={{position: [20, 3, 5], fov: 25}}
